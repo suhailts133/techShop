d60bab8b7f7339ce8c3cf4806017c32fc3f75905
Muhammed Suhail TS <126490542+suhailts133@users.noreply.github.com>
2025-02-25 10:41:48 +0530

    cookie 1.1.3

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(session({
     store,
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,  
+    saveUninitialized: false,  
     cookie: {
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
